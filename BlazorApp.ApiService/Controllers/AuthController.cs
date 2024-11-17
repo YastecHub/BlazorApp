3d090a8c3fc54006c721e1103e9c9f6445fa6616
yastec <yasiroyebo@gmail.com>
2024-11-17 17:09:03 +0100

    Create Refresh Token(API)

--- BlazorApp.ApiService/Controllers/AuthController.cs
+++ BlazorApp.ApiService/Controllers/AuthController.cs
@@ -16,23 +16,26 @@ namespace BlazorApp.ApiService.Controllers
         {
             if ((loginModel.UserName == "Admin" && loginModel.Password == "Admin") || (loginModel.UserName == "User" && loginModel.Password == "User"))
             {
-                var token = GenerateJwtToken(loginModel.UserName);
+                var token = GenerateJwtToken(loginModel.UserName, isRefreshToken: false);
+                var refreshToken = GenerateJwtToken(loginModel.UserName, isRefreshToken: true);
                 return Ok(new LoginResponseModel
                 {
                     Token = token,
+                    RefreshToken = refreshToken,
+                    TokenExpired = DateTimeOffset.UtcNow.AddHours(1).ToUnixTimeSeconds(),
                 });
             }
             return null;
         }
 
-        private string GenerateJwtToken(string userName)
+        private string GenerateJwtToken(string userName, bool isRefreshToken)
         {
             var claims = new[]
             {
             new Claim(ClaimTypes.Name, userName),
             new Claim(ClaimTypes.Role, userName == "Admin" ? "Admin" : "User")
         };
-            string secret = configuration.GetValue<string>("Jwt:Secret");
+            string secret = configuration.GetValue<string>($"Jwt:{(isRefreshToken ? "RefreshTokenSecret" : "Secret")}");
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
@@ -40,7 +43,7 @@ namespace BlazorApp.ApiService.Controllers
                 issuer:  "yasirola",
                 audience: "yasirola",
                 claims : claims,
-                expires: DateTime.UtcNow.AddHours(1),
+                expires: DateTime.UtcNow.AddHours(isRefreshToken ? 24 : 1),
                 signingCredentials: creds
                 );
             return new JwtSecurityTokenHandler().WriteToken(token);
--- BlazorApp.Models/Models/LoginResponseModel.cs
+++ BlazorApp.Models/Models/LoginResponseModel.cs
@@ -4,5 +4,6 @@
     {
         public string Token { get; set; }
         public long TokenExpired { get; set; }
+        public string RefreshToken { get; set; }
     }
 }
