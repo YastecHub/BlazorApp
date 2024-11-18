73d5fe277ef3d23048f4b2eb5081b3a160ac9390
yastec <yasiroyebo@gmail.com>
2024-11-18 17:15:47 +0100

    Save Refresh token to db

---
+++ BlazorApp.Models/Entities/RefreshTokenModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Models.Entities
+{
+    public class RefreshTokenModel
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public string RefreshToken { get; set; }
+        public UserModel UserModel { get; set; }
+    }
+}
---
+++ BlazorApp.Models/Entities/UserRoleModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Models.Entities
+{
+    public class UserRoleModel
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public int RoleID { get; set; }
+        public RoleModel Role { get; set; }
+        public UserModel User { get; set; }
+    }
+}
